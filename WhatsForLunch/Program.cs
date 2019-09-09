947de36efdea407d0250844be8dd210f9240f9e0
unknown <matthewaustinbell@gmail.com>
2019-09-08 21:24:44 -0500

    added git ignore

---
+++ WhatsForLunch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatsForLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
